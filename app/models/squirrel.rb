e9669c302993a19ecbf95ca0c0b12411328b234f
Zack Mance <zmance@gmail.com>
2015-02-05 12:34:43 -0500

    Fixed Styling Stuff

--- app/models/squirrel.rb
+++ app/models/squirrel.rb
@@ -2,4 +2,12 @@ class Squirrel < ActiveRecord::Base
   has_many :branches
   has_many :nuts
   has_many :trees, through: :branches
+
+  def nut_count
+    nuts.length
+  end
+
+  def tree_count
+    trees.length
+  end
 end
