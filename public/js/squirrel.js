d7c40613c5dca03018282b05c68f6c5644de9728
Zack Mance <zmance@gmail.com>
2015-02-11 10:46:12 -0500

    Ashi taugth me how to really do MVC

--- public/js/squirrel.js
+++ public/js/squirrel.js
@@ -13,3 +13,33 @@ function Squirrel(params) {
 Squirrel.prototype.toJson = function(){
   return JSON.stringify( {name: this.name, tailLength: this.tailLength, treeMail: this.treeMail} );
 };
+
+Squirrel.prototype.save = function(){
+    $.ajax({
+      type    : 'put',
+      url     : "/squirrels/"+ this.id  +"/update.json",
+      data    : { name:        this.name, 
+                  tail_length: this.tailLength, 
+                  tree_mail:   this.treeMail },
+      success : function(){ console.log('updated'); },
+      error   : function(){ console.log('something went wrong');} 
+    });
+};
+
+
+Squirrel.all = function( then, fail  ){
+    $.ajax({
+      type    : "get",
+      url     : "/squirrels.json",
+      success : function(response){
+        
+        var squirrels = [];
+        $.each(response, function(x,item){   
+          squirrels.push( new Squirrel( item ));
+        }); 
+
+        then(squirrels);
+      },
+      error   : function(){ console.log("something went wrong");}
+    });
+}
--- public/js/squirrelController.js
+++ public/js/squirrelController.js
@@ -1,35 +1,10 @@
+var squirrelList;
+
 var squirrelController = function(){
-  
-  var squirrels = [];
 
-  var getSquirrels = function(){  
-    $.ajax({
-      type    : "get",
-      url     : "/squirrels.json",
-      success : function(response){
-        $.each(response, function(x,item){   
-          squirrels.push( new Squirrel( item ));
-        }); 
-      },
-      error   : function(){ console.log("Something Went Wrong");}
-    });
-  };
-
-  var putSquirrel = function(squirrel){
-    $.ajax({
-      type    : 'put',
-      url     : "/squirrels/"+ squirrel.id  +"/update.json",
-      data    : { name:        squirrel.name, 
-                  tail_length: squirrel.tailLength, 
-                  tree_mail:   squirrel.treeMail },
-      success : function(){ console.log('updated'); },
-      error   : function(){ console.log('something went wrong');} 
-    });
-  };
+     Squirrel.all(function( squirrels ){
+       squirrelList = squirrels;
+     });
   
-  return {
-    index:  getSquirrels,
-    update: putSquirrel,
-    squirrels: squirrels
-  }
+   //html content editble / contentInput event wireup  
 }();
