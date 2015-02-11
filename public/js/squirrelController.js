c2723d01afd5af940134a94bd1f060e1becc397b
Zack Mance <zmance@gmail.com>
2015-02-11 16:09:10 -0500

    Adds basic controller actions

--- public/js/squirrelController.js
+++ public/js/squirrelController.js
@@ -1,9 +1,36 @@
-var squirrelList;
-
 var squirrelController = function(){
 
-     Squirrel.all(function( squirrels ){
-       squirrelList = squirrels;
-     });
+  var getSquirrels = function(){
+    Squirrel.all(function(squirrels){
+      console.log(squirrels);
+    });
+  };
+
+  var getSquirrel = function(id){
+    var squirrel;
+    Squirrel.find(id,function(squirrel){
+      console.log(squirrel);
+    });
+  };
+
+  var deleteSquirrel = function( squirrel ){
+    squirrel.destroy(function(){
+      console.log(squirrel.name + ": Deleted");
+    });  
+  }
+
+  var createSquirrel = function( params ){
+    squirrel = new Squirrel(param);
+    squirrel.save(function(id){
+      squirrel.id = id;    
+      console.log(squirrel);
+    });
+  }
   
+  return{
+    index:    getSquirrels,
+    show:     getSquirrel,
+    destroy:  deleteSquirrel
+  }
+
 }();
