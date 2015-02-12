3e45b7fb696013d2b7606714d3e9079b387d8c2a
Zack Mance <zmance@gmail.com>
2015-02-11 22:08:16 -0500

    replaces erb with replaceable strings

--- public/js/squirrelsIndex.js
+++ public/js/squirrelsIndex.js
@@ -1,28 +1,26 @@
 squirrelsIndex = function(){
 
   var html = "" +
-    "<div class='squirrel' data-squirrel-id='7'>" +
+    "<div class='squirrel' data-squirrel-id='{id}'>" +
      "<div class='name'>" +
-        "<%= squirrel.name %> " +
+        "{name} " +
       "</div>" +
       "<div class='counts'>" +
         "<div class='item'>" +
           "<label>Nuts:</label>" +
-          "<span class='value'>  <%= squirrel.nut_count %></span>" +
+          "<span class='value'>{nut_count}</span>" +
         "</div>" +
         "<div class='item'>" +
           "<label>Trees:</label>" +
-          "<span class='value'><%= squirrel.tree_count %></span>" +
+          "<span class='value'>{tree_count}</span>" +
         "</div>" +
       "</div>" +
       "<div class='actions'>" +
-        "<a class='btn' href='/squirrels/<%= squirrel.id %>/nuts'>nuts</a>" +
-        "<a class='btn' href='/squirrels/<%= squirrel.id %>/trees'>trees</a>" +
+        "<a class='btn' href='/squirrels/{id}/nuts'>nuts</a>" +
+        "<a class='btn' href='/squirrels/{id}/trees'>trees</a>" +
         "<a id='delete' class='btn' href='#'>delete</a>" +
       "</div>" +
     "</div> ";
 
-    
-     
     return{ html: html}
 }();
